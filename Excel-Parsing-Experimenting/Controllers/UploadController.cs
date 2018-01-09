9b3f45339d27228719e0a7c2baff414c950fefe9
Bronze Brown <dhamon.nightstar@gmail.com>
2018-01-09 10:54:20 -0500

    Starting work on parsing Firbit data

---
+++ Excel-Parsing-Experimenting/Controllers/UploadController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExcelDataReader;
+using Excel_Parsing_Experimenting.Models;
+
+namespace Excel_Parsing_Experimenting.Controllers
+{
+    public class UploadController : Controller
+    {
+        //// GET: Upload
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult ImportFitbitData(HttpPostedFile file)
+        {
+            var viewModel = new FitbitData();
+            if (file.ContentLength > 0)
+            {
+                var reader = ExcelReaderFactory.CreateBinaryReader(file.InputStream);
+                
+            }
+            return View()
+        }
+    }
+}
---
+++ Excel-Parsing-Experimenting/Models/FitbitData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Excel_Parsing_Experimenting.Models
+{
+    public class FitbitData
+    {
+        public int ID { get; set; }
+        public List<BodyDataEntry> BodyDataEntries { get; set; }
+        public List<SleepDataEntry> SleepDataEntries { get; set; }
+        public List<ActivityDataEntry> ActivityDataEntries { get; set; }
+    }
+
+    public class BodyDataEntry
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Weight { get; set; }
+        public double BMI { get; set; }
+        public double Fat { get; set; }
+    }
+
+    public class ActivityDataEntry
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public int CaloriesBurned { get; set; }
+        public int Steps { get; set; }
+        public double Distance { get; set; }
+        public int Floors { get; set; }
+        public int MinutesSedentary { get; set; }
+        public int MinutesLightlyActive { get; set; }
+        public int MinutesFairlyActive { get; set; }
+        public int MinutesVeryActive { get; set; }
+        public int ActivityCalories { get; set; }
+    }
+
+    public class SleepDataEntry
+    {
+        public int ID { get; set; } 
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int MinutesAsleep { get; set; }
+        public int MinutesAwake { get; set; }
+        public int NumberOfAwaakenings { get; set; }
+        public int TimeInBed { get; set; }
+        public int MinutesREMSleep { get; set; }
+        public int MinutesLightSleep { get; set; }
+        public int MinutesDeepSleep { get; set; }
+    }
+}
